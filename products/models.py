43894493bda766a76b0abb3f64b182edff0b55bc
Dhvani-intwala <intdhvani2627@gmail.com>
2023-08-31 16:29:10 +0000

    Add a products and categories

--- products/models.py
+++ products/models.py
@@ -1,3 +1,35 @@
+
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Category model"""
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    """Product model"""
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    books_type = models.CharField(max_length=254)
+    auth_name = models.CharField(max_length=254)
+    description = models.TextField()
+    year = models.PositiveIntegerField(null=False, blank=False)
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=False,
+                                blank=False)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
+                                 blank=True)
+    image = models.ImageField(null=True, blank=True)
+    stock = models.PositiveIntegerField(null=False, blank=False)
+
+    def __str__(self):
+        return self.name
