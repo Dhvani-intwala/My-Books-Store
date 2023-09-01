1188861b66f8a28a3e4a6797d5324d024e39da48
Dhvani-intwala <intdhvani2627@gmail.com>
2023-09-01 08:20:33 +0000

    Costomized admin

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,28 @@ from .models import Product, Category
 
 # Register your models here.
 
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    """Class for displaying products in admin panel"""
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'year',
+        'price',
+        'rating',
+        'image',
+    )
+    ordering =('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """Class for displaying categories in admin panel"""
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
