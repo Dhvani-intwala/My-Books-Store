4fb14ac6b2c4647324c8ece9c0aed43a1a92c75b
Dhvani-intwala <intdhvani2627@gmail.com>
2023-10-10 15:06:35 +0000

    Add hosts herokuapp and localhost

--- mybooksstore/settings.py
+++ mybooksstore/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-dhvani-intwala-my-books-xpobz1qk3j.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['my-books-store.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -123,16 +123,18 @@ WSGI_APPLICATION = 'mybooksstore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
