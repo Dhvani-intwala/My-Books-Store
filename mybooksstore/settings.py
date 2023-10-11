50e2b9d8ca8a525ed2f6d6b8ebff3bb19b9455da
Dhvani-intwala <intdhvani2627@gmail.com>
2023-10-11 13:48:01 +0000

    Add allowed host heroku link

--- mybooksstore/settings.py
+++ mybooksstore/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['my-books-store-6b01a57c6bd1.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = [ 'my-books-store-6b01a57c6bd1.herokuaap.com', 'localhost']
 
 
 # Application definition
