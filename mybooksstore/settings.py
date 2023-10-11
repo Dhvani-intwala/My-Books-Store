980489e2e179f4912d508424048816f917225109
Dhvani-intwala <intdhvani2627@gmail.com>
2023-10-11 13:22:37 +0000

    Debug False

--- mybooksstore/settings.py
+++ mybooksstore/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['my-books-store-6b01a57c6bd1.herokuapp.com', 'localhost']
 
