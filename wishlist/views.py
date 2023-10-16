0caf27d0392dc5acf76f1908cf7b62194a396ac7
Dhvani-intwala <intdhvani2627@gmail.com>
2023-10-16 08:49:30 +0000

    Add Terms & Conditions link

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from products.models import Product
-from .models import WishList
+# from wishlist.models import WishList
 from django.contrib import messages
 
 # Create your views here.
@@ -12,25 +12,25 @@ def wishlist(request):
     return render(request, 'wishlist/wishlist.html')
 
 
-# def Add_to_wishlist(request, product_id):
-#     """ A view that provides a form for creating a new entry in
-#     WishlistLine """
+def Add_to_wishlist(request, product_id):
+    """ A view that provides a form for creating a new entry in
+    WishlistLine """
 
-#     product = get_object_or_404(Product, pk=product_id)
+    product = get_object_or_404(Product, pk=product_id)
 
-#     context = {
-#         'product': product,
-#     }
+    context = {
+        'product': product,
+    }
 
-#     if not request.user.is_authenticated:
-#         messages.error(
-#             request, "You must be logged in to add items to your wishlist.")
-#         return redirect('login')
-#     else:
-#         # Create a new entry in the user's wishlist for the product
-#         wishlist_item = WishList(user=request.user, product=product)
-#         wishlist_item.save()
-#         messages.success(
-#             request, f"{product.name} has been added to your wishlist.")
+    if not request.user.is_authenticated:
+        messages.error(
+            request, "You must be logged in to add items to your wishlist.")
+        return redirect('login')
+    else:
+        # Create a new entry in the user's wishlist for the product
+        wishlist_item = WishList(user=request.user, product=product)
+        wishlist_item.save()
+        messages.success(
+            request, f"{product.name} has been added to your wishlist.")
 
-#     return redirect('wishlist') 
+    return redirect('wishlist') 
