496cc35f5241de56a6ed6bebc748364987384698
Dhvani-intwala <intdhvani2627@gmail.com>
2023-10-24 15:29:37 +0000

    Update product managmenet page

--- products/forms.py
+++ products/forms.py
@@ -11,16 +11,39 @@ class ProductForm(forms.ModelForm):
         fields = '__all__'
     
     category = forms.ChoiceField()
-    image = forms.ImageField(label='Image',
-                            required=False, widget=CustomClearableFileInput)
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
     year = forms.IntegerField(min_value=0,
                               max_value=datetime.datetime.now().year)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        placeholders = {
+            'category': 'Category',
+            'name': 'Name',
+            'sku': 'Sku',
+            'books_type': 'Books type',
+            'auth_name': 'Auth name',
+            'description': 'Description',
+            'year': 'Year',
+            'price': 'price',
+            'rating': 'Rating',
+            'stock': 'Stock',
+            
+        }
+
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = friendly_names
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black rounded-0'
+        for field in self.fields:
+            if field != 'country' and field != 'category'\
+                 and field != 'is_deluxe' and field != 'image':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].widget.attrs['class'] = ('border-black '
+                                                            'rounded-0 '
+                                                            'contact-form-input')
+                self.fields[field].label = False
