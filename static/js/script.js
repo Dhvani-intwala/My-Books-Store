3b28fb1b4f652cb584411371c362d967fa59bdb1
Dhvani-intwala <intdhvani2627@gmail.com>
2023-11-05 19:07:33 +0000

    Add jquery for active navbar

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,59 @@
- // ----------------------------- ADD ACTIVE CLASS TO THE CURRENT LINK ---------------------------
-var header = document.getElementById("main-nav");
-var btns = header.getElementsByClassName("nav-item");
-console.log(btns);
-for (var i = 0; i < btns.length; i++) {
-     btns[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("nav-tabs");
-        console.log(current);
-        current[0].className = current[0].className.replace(" nav-tabs", "");
-        this.className += " nav-tabs";
-    });
-}
-
 jQuery(document).ready(function($) {
     //---------------- PRELOADER -------------
     if ($("#preloader")) {
         $("#preloader").remove();
+    }
+    var pathname = window.location.pathname;
+    var searchQuery = window.location.search;
+    var pathnameArr = pathname.split('/');
+    var searchQueryArr = searchQuery.split('=')
+    console.log(searchQueryArr + ' ' + pathnameArr)
+    if (searchQuery) {
+    var editPath = '/' + pathnameArr[1] + '/' + searchQueryArr[0] + '/';
+    }
+    else{
+    var editPath = '/' + pathnameArr[1] + '/'
+    }
+    console.log("here "+editPath)
+    if  (window.location.pathname === "/") {
+        var currentActive = document.getElementsByClassName("active");
+        currentActive[0].className = currentActive[0].className.replace(" active","");
+        var homeTab = document.getElementById("Home-link");
+        homeTab.className += " active"
+        homeTab.style.borderBottom = "2px solid #fff";
+    }
+    else if (editPath == '/products/') {
+        var currentActive = document.getElementsByClassName("active");
+        currentActive[0].className = currentActive[0].className.replace(" active","");
+        var allProducts = document.getElementById("all-products-link");
+        allProducts.className += " active"
+        allProducts.style.borderBottom = "2px solid #fff";
+    }
+    else if (editPath === "/products/?category/") {
+        editPath += searchQueryArr[1]
+        console.log('here')
+        console.log(editPath)
+        if(editPath === "/products/?category/New%20Arrivals") {
+            var currentActive = document.getElementsByClassName("active");
+            currentActive[0].className = currentActive[0].className.replace(" active","");
+            var special = document.getElementById("specials-link");
+            special.className += " active"
+            special.style.borderBottom = "2px solid #fff";
+        }
+        else {
+            var currentActive = document.getElementsByClassName("active");
+            currentActive[0].className = currentActive[0].className.replace(" active","");
+            var bookLink = document.getElementById("all-products-links");
+            bookLink.className += " active"
+            bookLink.style.borderBottom = "2px solid #fff";
+        }
+    }
+    else if (window.location.pathname === "/contactus/") {
+        var currentActive = document.getElementsByClassName("active");
+        currentActive[0].className = currentActive[0].className.replace(" active","");
+        var contactus = document.getElementById("contactus");
+        contactus.className += " active"
+        contactus.style.borderBottom = "2px solid #fff";
+
     }
 });
