44a9f8051146ee26a35c7d2a72359dd58430093d
Dhvani-intwala <intdhvani2627@gmail.com>
2023-12-04 12:06:43 +0000

    Remove slash in meta tag

--- contactus/tests.py
+++ contactus/tests.py
@@ -1,16 +1,23 @@
-from django.test import TestCase
-from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
 from django.urls import reverse
-from .models import ContactMessage
+from .forms import ContactForm
+
 
 class ContactUsViewTest(TestCase):
-    def test_contact_form_submission(self):
-        # Test POST request with valid data
-        response = self.client.post(reverse('contact_us'), {
-            'name': 'John Doe',
-            'email': 'johndoe@example.com',
-            'phone': '1234567890',
-            'subject': 'Test Subject',
-            'message': 'Test Message',
-        })
+    def setUp(self):
+        self.client = Client()
 
+    def test_contact_us_view(self):
+        response = self.client.get(reverse('contact_us'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contactus/contact_us.html')
+     
+    def test_contact_us_form_submission(self):
+        data = {
+            'name': 'User',
+            'email': 'user@example.com',
+            'phone': '017642454232',
+            'message': 'Test message'
+        }
+        response = self.client.post(reverse('contact_us'), data)
+        self.assertEqual(response.status_code, 302)
